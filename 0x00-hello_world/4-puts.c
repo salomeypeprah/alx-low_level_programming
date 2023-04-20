6f51a4cf617a22d3fd081f3d6d5fbd8c96f89620
salomeypeprah <sapeprah2@gmail.com>
2023-04-20 06:08:01 -0700

    betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A proram that prints exactly puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle")
+return (0);
+}
+	
