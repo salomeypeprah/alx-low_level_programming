2354df8ed1841367c82e2e90a1cdd2f786248846
salomeypeprah <sapeprah2@gmail.com>
2023-04-21 05:03:32 -0700

    hello print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that prints exactly using printf function
+ * main - Entry point
  * Return: 0 (Success)
  */
-
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 
