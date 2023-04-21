b5df57c75dfa169325c7b889d67b73c25e0a9823
salomeypeprah <sapeprah2@gmail.com>
2023-04-21 04:34:30 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - A proram that prints exactly puts function
+ * main - Entry point
+ *
  * Return: 0 (success)
-**/
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
+puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }
-	
