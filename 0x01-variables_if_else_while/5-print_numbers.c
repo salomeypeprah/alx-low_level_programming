e7a397785ebd86e03e6a06adfc57706af760d506
salomeypeprah <sapeprah2@gmail.com>
2023-04-24 02:55:30 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+putchar(a + '0');
+}
+putchar('\n');
+return (0);
+}
+
