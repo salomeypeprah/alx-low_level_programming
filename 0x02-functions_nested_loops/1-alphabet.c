7050aadf6b2603c89e4af2602dfff1d624300229
salomeypeprah <sapeprah2@gmail.com>
2023-04-25 05:59:08 -0700

    insonmia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - print all letters in lowercase
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+int main(void)
+{
+	char alphabeth;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		print_alphabeth
+
+		_putchar(alphabet);
+
+	_putchar('\n');
+
+	return (0);
+}
