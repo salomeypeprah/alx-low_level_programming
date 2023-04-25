1d7d6baf5efc6f56c31775f32c39ba5e3349a765
salomeypeprah <sapeprah2@gmail.com>
2023-04-25 11:32:48 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main -  entry point.
+ *Return: Always 0
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+
