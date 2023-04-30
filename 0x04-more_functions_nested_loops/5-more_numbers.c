b7a0eb218b7467f4c3971caf00cea3599b1c1e5b
salomeypeprah <sapeprah2@gmail.com>
2023-04-30 10:45:46 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * universal language - a function that print more numbers
+ *Return: Always (0)
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+return: (0);
+}
