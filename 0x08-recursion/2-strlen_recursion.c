b337920193bcfe5a5fc7c018964e55afa52e912d
salomeypeprah <sapeprah2@gmail.com>
2023-05-10 12:10:05 -0700

    dream together

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _strlen_recursion -a function that Returns the length of a string.
+* @s: The string to be counted
+*
+* Return: interger value.
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+return;
+response += _strlen_recursion(s + 1);
+	}
+return (response);
+}
+
