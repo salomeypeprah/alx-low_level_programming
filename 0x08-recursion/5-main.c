f01c4f861ef998e178fd3fb50458c746fbe2c0a4
salomeypeprah <sapeprah2@gmail.com>
2023-05-11 02:21:50 -0700

    inception impossible

--- 0x08-recursion/5-main.c
+++ 0x08-recursion/5-main.c
@@ -10,53 +10,6 @@ int main(void)
 {
     int r;
 
-    r = _sqrt_recursion(1);
-    printf("%d\n", r);
-    r = _sqrt_recursion(1024);
-    printf("%d\n", r);
-    r = _sqrt_recursion(16);
-    printf("%d\n", r);
-    r = _sqrt_recursion(17);
-    printf("%d\n", r);
-    r = _sqrt_recursion(25);
-    printf("%d\n", r);
-    r = _sqrt_recursion(-1);
-    printf("%d\n", r);
-    return (0);
-}
-julien@ubuntu:~/0x08. gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-main.c 5-sqrt_recursion.c -o 5-sqrt
-julien@ubuntu:~/0x08. Recursion$ ./5-sqrt 
-1
-32
-4
--1
-5
--1
-julien@ubuntu:~/0x08. Recursion$ 
-Repo:
-
-GitHub repository: alx-low_level_programming
-Directory: 0x08-recursion
-File: 5-sqrt_recursion.c
-   
-6. Inception. Is it possible?
-mandatory
-Write a function that returns 1 if the input integer is a prime number, otherwise return 0.
-
-Prototype: int is_prime_number(int n);
-julien@ubuntu:~/0x08. Recursion$ cat 6-main.c
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int r;
-
     r = is_prime_number(1);
     printf("%d\n", r);
     r = is_prime_number(1024);
