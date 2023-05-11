891131450dda6543c784166c861c29057c0f5ba0
salomeypeprah <sapeprah2@gmail.com>
2023-05-11 01:53:16 -0700

    strange dreams

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,20 @@
 #include "main.h"
 /**
-* _strlen_recursion -a function that Returns the length of a string.
+* _strlen_recursion - a function that Returns the length of a string.
 * @s: The string to be counted
 *
 * Return: interger value.
 */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+int response = 0;
+
+	if (*s)
 	{
-return;
-response += _strlen_recursion(s + 1);
+		response++;
+		response += _strlen_recursion(s + 1);
 	}
-return (response);
+
+	return (response);
 }
 
