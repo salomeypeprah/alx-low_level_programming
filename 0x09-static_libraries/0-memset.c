fe258e0f0150bb0a6cc3de1dd1a4ed97e1142651
salomeypeprah <sapeprah2@gmail.com>
2023-05-12 17:28:57 -0700

    necessities of life

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset - a function that fills a memory with a constant byte
+ * _memset - a function that fills a memory with a constant byte 
  * @s: starting address of memory to be filled
  * @b: the desired value
  * @n: number of bytes to be changed
--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -28,3 +28,4 @@ char *_strncat(char *dest, char *src, int n)
 
 	return (dest);
 }
+
